f7ff60c39bba97b7040df2e5a1bccf1597bfe940
Mike Boyer <boyer.mike.e@gmail.com>
2017-07-02 18:44:52 -0400

    Monster
    Adding a robot monster to follow you until you shoot it with the ray gun

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+    public float speed;
+    private GameObject monsterBody;
+
+    void Awake()
+    {
+        monsterBody = GetComponent<GameObject>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        monsterBody.transform.position = new Vector3(speed, 0, 0);
+	}
+}
---
+++ Assets/Scripts/ZapperScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZapperScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //when a certain trigger happens, enable the gun (tag - Zapper)
+
+
+    //upon clicking a monster, enable the gun to fire (tag - ZapBlast)
+    //when monster gone, destroy Zapper and ZapBlast
+
+}
