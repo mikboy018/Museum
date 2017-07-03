0a83d8d192880093229e0c56e30425378d3b598e
Mike Boyer <boyer.mike.e@gmail.com>
2017-07-02 20:50:37 -0400

    Working shooter implemented
    Time to add some sounds and particle effects for the robot, then just text

--- Assets/Scripts/ZapperScript.cs
+++ Assets/Scripts/ZapperScript.cs
@@ -1,23 +1,102 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ZapperScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Camera fpsCam;
+    public float waitTime;
+    public GameObject[] zapperArray;
+    public GameObject[] zapBlastArray;
+    public Transform weapon;
+   
 
-    //when a certain trigger happens, enable the gun (tag - Zapper)
+    // Use this for initialization
+    void Start() {
+        string type = "ZapBlast";
+        zapBlastArray = GameObject.FindGameObjectsWithTag(type);
+        foreach (GameObject go in zapBlastArray)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+            zap();
+            Debug.Log("Bang!");
+                      
+        }
+    }
 
 
-    //upon clicking a monster, enable the gun to fire (tag - ZapBlast)
-    //when monster gone, destroy Zapper and ZapBlast
+    void zap()
+    {
+        //upon clicking a monster, enable the gun to fire (tag - ZapBlast)
+        //when monster gone, destroy Zapper and ZapBlast
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+            if(hit.transform.name == "RobotMonster")
+            {
+                Destroy(GameObject.Find("RobotMonster"));
+                Debug.Log("Got him!");
+                StartCoroutine(flash());
+                StartCoroutine(disableWeapon());
+            }
+            if (hit.transform.name != "Floor")
+            {
+                GunBlast();
+                Debug.Log("Missed!");
+            }
+        }
+    }
 
+    void GunBlast()
+    {
+        
+        foreach (GameObject go in zapBlastArray)
+        {
+            go.SetActive(true);
+        }
+
+        StartCoroutine(flash());
+       /*
+        foreach (GameObject go in zapBlastArray)
+        {
+            go.SetActive(false);
+        }
+        Debug.Log("no more blast"); */
+    }
+
+    void stopBlast()
+    {
+        foreach (GameObject go in zapBlastArray)
+        {
+            go.SetActive(false);
+        }
+        Debug.Log("no more blast");
+    }
+
+    public IEnumerator flash()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Debug.Log("waiting");
+        stopBlast();
+    }
+    public IEnumerator disableWeapon()
+    {
+        zapperArray = GameObject.FindGameObjectsWithTag("Zapper");
+        foreach (GameObject go in zapperArray)
+        {
+            yield return new WaitForSeconds(waitTime*5f);
+            go.transform.position += new Vector3(weapon.position.x, -.125f, weapon.position.z);
+            go.SetActive(false);
+        }
+    }
 }
